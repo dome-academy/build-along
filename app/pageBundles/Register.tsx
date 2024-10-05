8bc6cdc2c19259a7b3e71af8f9cc3f5a71f8e44b
Emeka Allison <emekaallison4@gmail.com>
2024-10-05 05:35:03 +0100

    Feat: Register section

---
+++ app/pageBundles/Register.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <section
+      id='register'
+      className='w-full py-12 pb-16 md:pb-24 px-4 bg-white text-black font-[family-name:var(--font-body)]'
+    >
+      <div className='relative z-10 max-w-3xl mx-auto px-4 text-center md:px-8 bg-black grid-background text-neutral-50 py-12 md:py-16 lg:py-20 rounded-xl'>
+        <div className='max-w-xl mx-auto'>
+          <p className='text-yellow-400 text-3xl font-semibold sm:text-4xl uppercase font-[family-name:var(--font-display)]'>
+            Become an expert in web development
+          </p>
+          <p className='text-blue-100 mt-3'>
+            Learn web development in 8 weeks by building alongside a community
+            of learners. Register for #BuildAlong now!
+          </p>
+        </div>
+        <div className='mt-4 md:mt-6'>
+          <a
+            className='rounded-full border border-solid border-transparent transition-colors inline-flex items-center justify-center bg-[#ededed] text-[#0a0a0a] gap-2 hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+            href='#details'
+          >
+            Register now
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
