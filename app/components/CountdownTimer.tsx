588bf77b11ad05bebe66c2c324df3223debbebde
Emeka Allison <emekaallison4@gmail.com>
2024-10-17 09:21:32 +0100

    Update countdown time

--- app/components/CountdownTimer.tsx
+++ app/components/CountdownTimer.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 
 const CountdownTimer: React.FC = () => {
   const calculateTimeLeft = () => {
-    const targetDate = new Date("2024-10-21T00:00:00").getTime(); // Set the target date
+    const targetDate = new Date("2024-10-19T23:59:59").getTime(); // Set the target date
     const currentDate = new Date().getTime();
     const difference = targetDate - currentDate;
 
@@ -39,7 +39,7 @@ const CountdownTimer: React.FC = () => {
   return (
     <div className='bg-yellow-400 text-neutral-800 p-8 flex flex-col justify-center md:items-center font-[family-name:var(--font-body)]'>
       <div className='text-2xl font-bold mb-4 font-[family-name:var(--font-display)] uppercase'>
-        Time Left Until #BuildAlong
+        Registration ends in:
       </div>
       <div className='text-2xl sm:text-4xl font-bold'>
         {`${timeLeft.days}d : ${timeLeft.hours}h : ${timeLeft.minutes}m : ${timeLeft.seconds}s`}
