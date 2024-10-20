27addd26cb35e6552f554c517d20f4b65d2eb164
Emeka Allison <emekaallison4@gmail.com>
2024-10-20 19:56:45 +0100

    Feat: Login page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
