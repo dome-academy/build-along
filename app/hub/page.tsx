096865a26c0d264fc249ce97afa27507ab7574e1
Emeka Allison <emekaallison4@gmail.com>
2024-10-27 09:10:38 +0100

    Fix more color issues

--- app/hub/page.tsx
+++ app/hub/page.tsx
@@ -15,7 +15,7 @@ const HubPage = async () => {
         <div className='mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
           <div className='flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between'>
             <div>
-              <h1 className='text-2xl font-bold text-gray-900 sm:text-3xl dark:text-white'>
+              <h1 className='text-2xl font-bold sm:text-3xl text-white'>
                 Hello, {currentStudent?.name as string}{" "}
               </h1>
 
