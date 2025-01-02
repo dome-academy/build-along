a29ecab262cc64520611c1b5eacaef7a9ae0be3c
Emeka Allison <emekaallison4@gmail.com>
2025-01-02 18:21:29 +0100

    Add BuildAlong'S25 waitlist link

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,8 @@ export default function Home() {
             </Link>
             <a
               className='rounded-full border border-solid border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44'
-              href='#register'
+              href='https://tally.so/r/3NYAWl'
+              target='_blank'
             >
               Join Waitlist for S&apos;25
             </a>
